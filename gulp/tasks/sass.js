e33eb1b06403c050f8abd168df4016c9bef5296d
Ben <benweatherman@gmail.com>
2014-08-01 01:24:18 -0500

    Add bourbon and neat, start basic styling

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -2,7 +2,14 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
+    var paths = [].concat(require('node-bourbon').includePaths, require('node-neat').includePaths);
+
     return gulp.src('./src/sass/*.scss')
-        .pipe(sass({sourceComments: 'map', sourceMap: 'sass', style: 'compact'}))
+        .pipe(sass({
+            sourceComments: 'map',
+            sourceMap: 'sass',
+            style: 'compact',
+            includePaths: paths
+        }))
         .pipe(gulp.dest('./dist/css'));
 });
