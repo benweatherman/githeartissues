62a6f9bee2362f28c6f83f93104768e0f86753ea
Matt Omohundro <matt@ordoro.com>
2014-08-07 12:11:21 -0500

    First pass at styling the UI
    Welp, everything’s pretty much styled now. Here are some fun bits:
    • I went for a very neutral, non-distracting color palette (since we’ll
    be staring at this a lot)
    • Using “Arimo” for the typeface and pulling it straight from the
    Google fonts CDN, so hopefully that’s cool.
    • I’m using ems instead of percentages so that empty lists don’t break
    and fall under other lists
    • There’s a fixed min-width for overall section container. If the
    viewport isn’t wide enough, users can’t horizontally scroll to see all
    of the lists. This is similar to what Trello does with their layout
    • Assign issue popover is workin and twerkin
    • Search bar expands on focus (like a rock hard weenis)
    
    I still need to do the markup for a credentials modal as well find a
    spot for call-to-action to load said modal.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,6 +3,5 @@ var gulp = require('gulp');
 gulp.task('watch', function() {
     gulp.watch('./src/js/**', ['build-js']);
     gulp.watch('./src/sass/**', ['build-css']);
-    gulp.watch('./src/images/**', ['build-icons']);
     gulp.watch('./src/html/**', ['build-html']);
 });
